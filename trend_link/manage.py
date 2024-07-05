9c26c666dbcbd0c05153a070c7c003ac474eca40
BGabor01 <busnes14@gmail.com>
2024-07-05 14:22:03 +0200

    manage.py: Setting setting module based on the environment

--- trend_link/manage.py
+++ trend_link/manage.py
@@ -6,7 +6,9 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trend_link.settings')
+    environment = os.getenv('ENVIRONMENT', 'development')
+    settings_module = f"trend_link.settings.{environment}"
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
