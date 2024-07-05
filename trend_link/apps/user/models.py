0ec20057b58b0d59c14f4584df4000b148583156
BGabor01 <busnes14@gmail.com>
2024-07-05 21:25:11 +0200

    models: Created UserProfile model

--- trend_link/apps/user/models.py
+++ trend_link/apps/user/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    bio = models.CharField(blank=True, max_length=255)
+    birth_date = models.DateField(null=True, blank=True)
+    profile_picture = models.ImageField(
+        upload_to="profile_pictures/",
+        default="profile_pictures/default.jpg",
+    )
+
+    def __str__(self) -> str:
+        return self.user.username
