aca99b5e9b86810b36b01410d6304240ac51e388
BGabor01 <busnes14@gmail.com>
2024-07-05 21:21:00 +0200

    enrtypoints.sh: Collect static files

--- trend_link/entrypoint.sh
+++ trend_link/entrypoint.sh
@@ -5,7 +5,9 @@ set -e
 echo "===================== Migration session started ====================="
 python manage.py makemigrations
 python manage.py migrate
-
 echo "===================== Migration session finished ====================="
+
+python manage.py collectstatic --no-input
+
 echo "===================== Starting application ====================="
 exec "$@"
