f82dbb0a82f2b37978da6adf00c92e4ca3d3cbc1
BGabor01 <busnes14@gmail.com>
2024-07-06 14:41:40 +0200

    user.views: Created singup and home view

--- trend_link/apps/user/views.py
+++ trend_link/apps/user/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib.auth import login, authenticate
 
-# Create your views here.
+from .forms import SingUpForm
+
+
+class SingUpView(View):
+    form_class = SingUpForm
+    template_name = "user/singup.html"
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {"form": form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save()
+            raw_password = form.cleaned_data.get("password1")
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect("home")
+        return render(request, self.template_name, {"form": form})
+
+
+class HomeView(View):
+    def get(self, request):
+        return render(request, "user/home.html")
