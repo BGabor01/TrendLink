ead92e7840cddacd9066e09f4b00fbe142dfd208
BGabor01 <busnes14@gmail.com>
2024-07-06 22:45:44 +0200

    user.urls: Added members path and changed profile

--- trend_link/apps/user/urls.py
+++ trend_link/apps/user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import SingUpView, LoginView, ProfileView, LogoutView
+from .views import SingUpView, LoginView, ProfileView, LogoutView, ListMembers
 
 urlpatterns = [
     path("singup/", SingUpView.as_view(), name="singup"),
     path("login/", LoginView.as_view(), name="login"),
-    path("profile/", ProfileView.as_view(), name="profile"),
+    path("profile/<int:id>", ProfileView.as_view(), name="profile"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("members/", ListMembers.as_view(), name="members"),
 ]
