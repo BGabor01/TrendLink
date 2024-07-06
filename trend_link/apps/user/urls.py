4dfd341fe7eee0b6c53ad5111daad819e960fdd2
BGabor01 <busnes14@gmail.com>
2024-07-06 22:47:31 +0200

    user.urls: Fixed members path's view

--- trend_link/apps/user/urls.py
+++ trend_link/apps/user/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import SingUpView, LoginView, ProfileView, LogoutView, ListMembers
+from .views import SingUpView, LoginView, ProfileView, LogoutView, ListMembersView
 
 urlpatterns = [
     path("singup/", SingUpView.as_view(), name="singup"),
     path("login/", LoginView.as_view(), name="login"),
     path("profile/<int:id>", ProfileView.as_view(), name="profile"),
     path("logout/", LogoutView.as_view(), name="logout"),
-    path("members/", ListMembers.as_view(), name="members"),
+    path("members/", ListMembersView.as_view(), name="members"),
 ]
