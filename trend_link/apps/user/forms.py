b84d26e4ba661b46273e62bee488818667c823c4
BGabor01 <busnes14@gmail.com>
2024-07-06 17:35:23 +0200

    user.forms: Removed help text from username, added LoginForm

--- trend_link/apps/user/forms.py
+++ trend_link/apps/user/forms.py
@@ -1,13 +1,28 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 class SingUpForm(UserCreationForm):
-    email = forms.EmailField(
-        max_length=254, help_text="Required. Enter a valid email address."
+    email = forms.EmailField(max_length=254)
+    username = forms.CharField(
+        max_length=254,
     )
 
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(
+        max_length=254,
+        widget=forms.TextInput(
+            attrs={"placeholder": "Username", "class": "form-control"}
+        ),
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={"placeholder": "Password", "class": "form-control"}
+        ),
+    )
