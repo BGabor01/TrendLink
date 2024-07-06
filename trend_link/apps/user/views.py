94dae496be0742eb17ffc395e6b02f1eec6685ca
BGabor01 <busnes14@gmail.com>
2024-07-06 17:35:54 +0200

    user.views: Created LoginView

--- trend_link/apps/user/views.py
+++ trend_link/apps/user/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth import login, authenticate
 
-from .forms import SingUpForm
+from .forms import SingUpForm, LoginForm
 
 
 class SingUpView(View):
@@ -19,6 +19,24 @@ class SingUpView(View):
             user = form.save()
             raw_password = form.cleaned_data.get("password1")
             user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect("home")
+        return render(request, self.template_name, {"form": form})
+
+
+class LoginView(View):
+    form_class = LoginForm
+    template_name = "user/login.html"
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {"form": form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
             login(request, user)
             return redirect("home")
         return render(request, self.template_name, {"form": form})
