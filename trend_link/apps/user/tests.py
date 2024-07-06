a68f0bb21338d8335970a95a66927284081772cf
BGabor01 <busnes14@gmail.com>
2024-07-06 16:54:38 +0200

    user.tests: Test profile creating

--- trend_link/apps/user/tests.py
+++ trend_link/apps/user/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+import pytest
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+@pytest.fixture
+def user(db):
+    return User.objects.create_user(username="testuser", password="password")
+
+
+@pytest.mark.django_db
+def test_profile_creating_signal(user):
+    from apps.user.models import UserProfile
+
+    profile = UserProfile.objects.get(user=user)
+
+    assert profile
