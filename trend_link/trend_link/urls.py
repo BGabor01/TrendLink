47f84edfa491c3c47cba2da3384eed761de435a9
BGabor01 <busnes14@gmail.com>
2024-07-06 17:38:07 +0200

    main urls: Fixed home page import (home page temporarily in user)

--- trend_link/trend_link/urls.py
+++ trend_link/trend_link/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls import static
-from apps.user.urls import HomeView
+from apps.user.views import HomeView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
