9ff01a4e6970b7558ad2b12b94163ce7fe17bcb4
BGabor01 <busnes14@gmail.com>
2024-07-06 17:35:33 +0200

    user.urls: Added login path

--- trend_link/apps/user/urls.py
+++ trend_link/apps/user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SingUpView, HomeView
+from .views import SingUpView, LoginView
 
 urlpatterns = [
     path("singup/", SingUpView.as_view(), name="singup"),
+    path("login/", LoginView.as_view(), name="login"),
 ]
