3c2df08c84403bb0af27e1c889946a325b678183
BGabor01 <busnes14@gmail.com>
2024-07-07 12:05:39 +0200

    user.urls: Using pk instead of id

--- trend_link/apps/user/urls.py
+++ trend_link/apps/user/urls.py
@@ -4,7 +4,7 @@ from .views import SingUpView, LoginView, ProfileView, LogoutView, ListMembersVi
 urlpatterns = [
     path("singup/", SingUpView.as_view(), name="singup"),
     path("login/", LoginView.as_view(), name="login"),
-    path("profile/<int:id>", ProfileView.as_view(), name="profile"),
+    path("profile/<int:pk>/", ProfileView.as_view(), name="profile"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("members/", ListMembersView.as_view(), name="members"),
 ]
