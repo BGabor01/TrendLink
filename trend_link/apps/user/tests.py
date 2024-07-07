6e91345ef0c825856bb367f14f04e39389bba566
BGabor01 <busnes14@gmail.com>
2024-07-07 12:44:46 +0200

    user.tests: Test IsUserProfileOwnerMixin

--- trend_link/apps/user/tests.py
+++ trend_link/apps/user/tests.py
@@ -1,5 +1,11 @@
 import pytest
 from django.contrib.auth.models import User
+from django.views import View
+from django.http import HttpResponse
+from django.test import RequestFactory
+from django.core.exceptions import PermissionDenied
+
+from apps.user.permissions import IsUserProfileOwnerMixin
 
 
 @pytest.fixture
@@ -14,3 +20,65 @@ def test_profile_creating_signal(user):
     profile = UserProfile.objects.get(user=user)
 
     assert profile
+
+
+class TestView(IsUserProfileOwnerMixin, View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("User is owner!")
+
+    def post(self, request, *args, **kwargs):
+        return HttpResponse("User is owner!")
+
+
+@pytest.fixture
+def other_user(db):
+    return User.objects.create_user(username="other", password="password")
+
+
+@pytest.fixture
+def factory():
+    return RequestFactory()
+
+
+def test_permission_denied_if_not_owner_POST(factory, other_user, user):
+    from apps.user.models import UserProfile
+
+    request = factory.get("/fake-url")
+    request.user = other_user
+    request.method = "POST"
+    profile = UserProfile.objects.get(user=user)
+
+    view = TestView.as_view()
+
+    with pytest.raises(PermissionDenied):
+        view(request, pk=profile.pk)
+
+
+def test_access_granted_GET(factory, other_user, user):
+    from apps.user.models import UserProfile
+
+    request = factory.get("/fake-url")
+    request.user = other_user
+    request.method = "GET"
+    profile = UserProfile.objects.get(user=user)
+
+    view = TestView.as_view()
+
+    response = view(request, pk=profile.pk)
+    assert response.status_code == 200
+
+
+def test_access_granted_POST(factory, user):
+    from apps.user.models import UserProfile
+
+    request = factory.get("/fake-url")
+    request.user = user
+    request.method = "POST"
+    profile = UserProfile.objects.get(user=user)
+
+    view = TestView.as_view()
+
+    response = view(request, pk=profile.pk)
+
+    assert response.status_code == 200
+    assert response.content.decode() == "User is owner!"
