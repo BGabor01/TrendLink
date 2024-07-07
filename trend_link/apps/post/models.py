65566517b60c2a1ae1a6be2bca0883b5bae9afaf
BGabor01 <busnes14@gmail.com>
2024-07-07 19:09:30 +0200

    post.models: Create model for Post, comment, like

--- trend_link/apps/post/models.py
+++ trend_link/apps/post/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="posts", related_query_name="posts"
+    )
+    text = models.CharField(max_length=255, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, upload_to="post_images/")
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True, db_index=True)
+
+    def __str__(self) -> str:
+        return f"Post by {self.user.username} on {self.created_at.strftime('%Y-%m-%d %H:%M')}"
+
+    def get_number_of_likes(self):
+        return self.likes.count()
+
+    def get_number_of_comments(self):
+        return self.comments.count()
+
+    class Meta:
+        app_label = "post"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments",
+        related_query_name="comments",
+    )
+    text = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"Comment on Post {self.post.pk}"
+
+    class Meta:
+        app_label = "post"
+
+
+class Like(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="likes", related_query_name="likes"
+    )
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="likes", related_query_name="likes"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"Like on Post: {self.post.pk} by {self.user.username}"
+
+    class Meta:
+        app_label = "post"
+        unique_together = ("user", "post")
