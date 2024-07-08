d3b80a7fe6a0be23dbb56b8e6ccd01212c09a499
BGabor01 <busnes14@gmail.com>
2024-07-08 12:45:46 +0200

    user.urls: Using DRF and frontend paths

--- trend_link/apps/user/urls.py
+++ trend_link/apps/user/urls.py
@@ -1,10 +1,41 @@
 from django.urls import path
-from .views import SingUpView, LoginView, ProfileView, LogoutView, ListMembersView
+from .views import (
+    LoginView,
+    UpdateProfileView,
+    RetrieveProfileView,
+    LogoutView,
+    ListMembersView,
+    SignUpView,
+)
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("singup/", SingUpView.as_view(), name="singup"),
-    path("login/", LoginView.as_view(), name="login"),
-    path("profile/<int:pk>/", ProfileView.as_view(), name="profile"),
+    path("api/singup/", SignUpView.as_view(), name="signup-api"),
+    path(
+        "signup/", TemplateView.as_view(template_name="user/signup.html"), name="signup"
+    ),
+    path("api/login/", LoginView.as_view(), name="login-api"),
+    path("login/", TemplateView.as_view(template_name="user/login.html"), name="login"),
+    path(
+        "api/profile/<int:pk>/update/",
+        UpdateProfileView.as_view(),
+        name="profile-update-api",
+    ),
+    path(
+        "api/profile/<int:pk>/",
+        RetrieveProfileView.as_view(),
+        name="profile-retrieve-api",
+    ),
+    path(
+        "profile/<int:pk>/",
+        TemplateView.as_view(template_name="user/profile.html"),
+        name="profile",
+    ),
     path("logout/", LogoutView.as_view(), name="logout"),
-    path("members/", ListMembersView.as_view(), name="members"),
+    path("api/members/", ListMembersView.as_view(), name="members-api"),
+    path(
+        "members/",
+        TemplateView.as_view(template_name="user/members.html"),
+        name="members",
+    ),
 ]
