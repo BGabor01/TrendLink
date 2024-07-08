11238c6702aa735a7d03de9f14468de692f76c22
BGabor01 <busnes14@gmail.com>
2024-07-08 12:37:39 +0200

    user.login.js: Js file for login

---
+++ trend_link/apps/user/static/user/js/login.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $('#loginForm').on('submit', function(e) {
+        e.preventDefault();
+        var $form = $(this);
+        var url = $form.data('url');
+        $.ajax({
+            type: 'POST',
+            url: url,
+            data: $form.serialize(),
+            headers: {
+                'X-CSRFToken': $('input[name="csrfmiddlewaretoken"]').val()
+            },
+            success: function(response) {
+                window.location.href = "/home";
+            },
+            error: function(response) {
+                var errors = response.responseJSON;
+                var errorMessage = '';
+                for (var field in errors) {
+                    errorMessage += errors[field] + ' ';
+                }
+                $('#error-message').text(errorMessage);
+            }
+        });
+    });
+});
