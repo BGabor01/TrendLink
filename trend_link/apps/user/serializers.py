403844b4e16d9904f5deda1d5a9337a1b018cebc
BGabor01 <busnes14@gmail.com>
2024-07-08 12:46:34 +0200

    user.serializers: Serializers for DRF views

---
+++ trend_link/apps/user/serializers.py
@@ -0,0 +1,65 @@
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from rest_framework import serializers
+
+
+class SignUpSerializer(serializers.ModelSerializer):
+    password1 = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+    def validate(self, data):
+        if data["password1"] != data["password2"]:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data["username"], email=validated_data["email"]
+        )
+        user.set_password(validated_data["password1"])
+        user.save()
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        username = data.get("username")
+        password = data.get("password")
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user:
+                if not user.is_active:
+                    raise serializers.ValidationError("User is deactivated.")
+                data["user"] = user
+            else:
+                raise serializers.ValidationError(
+                    "Unable to log in with provided credentials."
+                )
+        else:
+            raise serializers.ValidationError("Must include 'username' and 'password'.")
+
+        return data
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "email"]
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        from apps.user.models import UserProfile
+
+        model = UserProfile
+        fields = "__all__"
