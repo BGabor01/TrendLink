07c9ae6563d85763619b7ebd3b1c7c12d87b35b3
BGabor01 <busnes14@gmail.com>
2024-07-08 12:46:54 +0200

    user.permissions: Object level owner permission

--- trend_link/apps/user/permissions.py
+++ trend_link/apps/user/permissions.py
@@ -1,27 +1,10 @@
-from django.core.exceptions import PermissionDenied
-from django.shortcuts import get_object_or_404
+from rest_framework import permissions
 
 
-class UserIsOwner:
-    model = None
-    user_field = "user"
+class IsOwner(permissions.BasePermission):
 
-    def dispatch(self, request, *args, **kwargs):
-        if not self.model:
-            raise ValueError("Model attribute must be set")
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
 
-        obj = get_object_or_404(self.model, pk=kwargs["pk"])
-        if (
-            request.method not in ["GET", "HEAD", "OPTIONS", "TRACE"]
-            and getattr(obj, self.user_field) != request.user
-        ):
-            raise PermissionDenied
-
-        kwargs["obj"] = obj
-        return super().dispatch(request, *args, **kwargs)
-
-
-class IsUserProfileOwnerMixin(UserIsOwner):
-    from apps.user.models import UserProfile
-
-    model = UserProfile
+        return obj.user == request.user
