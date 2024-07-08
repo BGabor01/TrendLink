4dc1d9297b470affeb431cbc52ec1bcf86998b83
BGabor01 <busnes14@gmail.com>
2024-07-08 16:21:30 +0200

    user.members.js: Refactored for new response format

--- trend_link/apps/user/static/user/js/members.js
+++ trend_link/apps/user/static/user/js/members.js
@@ -1,30 +1,30 @@
-$(document).ready(function() {
+$(document).ready(function () {
     function loadProfiles(membersUrl, membersUrlApi, profileDetailsUrl, loginUrl) {
         $.ajax({
             url: membersUrlApi,
             type: 'GET',
-            success: function(response) {
+            success: function (response) {
                 var profilesList = $('#profiles-list');
                 profilesList.empty();
 
-                response.results.forEach(function(profile) {
-                    profileDetailsUrl = profileDetailsUrl.replace('0',profile.user.id )
+                response.results.forEach(function (profile) {
+                    profileDetailsUrl = profileDetailsUrl.replace('0', profile.id)
                     var profileCard = `
                         <div class="profile-card">
                             <div class="profile-picture">
-                                <img src="${profile.profile_picture}" alt="${profile.user.username}'s profile picture">
+                                <img src="${profile.profile.profile_picture}" alt="${profile.username}'s profile picture">
                             </div>
                             <div class="profile-details">
-                                <p><strong>Username:</strong> ${profile.user.username}</p>
-                                <p><strong>Email:</strong> ${profile.user.email}</p>
+                                <p><strong>Username:</strong> ${profile.username}</p>
+                                <p><strong>Email:</strong> ${profile.email}</p>
                                 <a href="${profileDetailsUrl}">View Profile</a>
                             </div>
                         </div>`;
-                    
+
                     profilesList.append(profileCard);
                 });
             },
-            error: function(response) {
+            error: function (response) {
                 if (response.status === 403) {
                     localStorage.setItem('redirectAfterLogin', membersUrl);
                     window.location.href = loginUrl;
