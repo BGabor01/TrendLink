4321835e7a7e1746d0c44b4ae9b184df1c4428b9
BGabor01 <busnes14@gmail.com>
2024-07-09 14:38:28 +0200

    post.urls:  Using DRF api path and templateviews for frontend

--- trend_link/apps/post/urls.py
+++ trend_link/apps/post/urls.py
@@ -1,8 +1,30 @@
 from django.urls import path
-from apps.post.views import PostView, ListPostView, CreatePostView
+from django.views.generic import TemplateView
+from apps.post.views import (
+    ListPostsView,
+    CreatePostView,
+    CreateCommentView,
+    DeleteCommentView,
+    UpdateCommentView,
+)
 
 urlpatterns = [
-    path("create/", CreatePostView.as_view(), name="create-post"),
-    path("list/", ListPostView.as_view(), name="list-post"),
-    path("<int:pk>/", PostView.as_view(), name="post"),
+    path("api/create/", CreatePostView.as_view(), name="create-post-api"),
+    path(
+        "create/",
+        TemplateView.as_view(template_name="post/post.html"),
+        name="create-post",
+    ),
+    path("api/list/", ListPostsView.as_view(), name="list-post-api"),
+    path("api/comment/create/", CreateCommentView.as_view(), name="create-comment-api"),
+    path(
+        "api/comment/<int:pk>/delete/",
+        DeleteCommentView.as_view(),
+        name="delete-comment-api",
+    ),
+    path(
+        "api/comment/<int:pk>/update/",
+        UpdateCommentView.as_view(),
+        name="update-comment-api",
+    ),
 ]
