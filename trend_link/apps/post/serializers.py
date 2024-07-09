fc2ae64de05af6a4984c6b0cce5892f085c9614f
BGabor01 <busnes14@gmail.com>
2024-07-09 14:37:45 +0200

    post.serializers: DRF serializers

--- trend_link/apps/post/serializers.py
+++ trend_link/apps/post/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
-from apps.post.models import Post
+from django.shortcuts import get_object_or_404
+
+from apps.post.models import Post, Comment
+from apps.user.serializers import UserSerializer, ProfileSerializer
 
 
 class EditCreatePostSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Post
+        fields = ["text", "image"]
 
     def validate(self, data):
         text = data.get("text")
@@ -15,6 +22,47 @@ class EditCreatePostSerializer(serializers.ModelSerializer):
 
         return data
 
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = "__all__"
+
+
+class ListPostsSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True, read_only=True)
+    user = UserSerializer(read_only=True)
+
     class Meta:
         model = Post
-        fields = ["text", "image"]
+        fields = "__all__"
+
+
+class CreateCommentSerializer(serializers.ModelSerializer):
+    post_id = serializers.IntegerField(write_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ["post_id", "text"]
+
+    def validate_post_id(self, value):
+        get_object_or_404(Post, id=value)
+        return value
+
+    def create(self, validated_data):
+        post_id = validated_data.pop("post_id")
+        post = get_object_or_404(Post, id=post_id)
+        comment = Comment.objects.create(post=post, **validated_data)
+        return comment
+
+    def to_representation(self, instance):
+        return CommentSerializer(instance).data
+
+
+class UpdateCommentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Comment
+        fields = ["text"]
