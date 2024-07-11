b687157b328e146670938c0359f2574973f85743
BGabor01 <busnes14@gmail.com>
2024-07-11 18:27:46 +0200

    post.serializers: Fixed CreateCommentSerializer, added has_liked field to ListPostsSerializer, created LikeSerializer, CreateLikeSerializer

--- trend_link/apps/post/serializers.py
+++ trend_link/apps/post/serializers.py
@@ -1,12 +1,9 @@
 from rest_framework import serializers
-from django.shortcuts import get_object_or_404
-
-from apps.post.models import Post, Comment
-from apps.user.serializers import UserSerializer, ProfileSerializer
+from apps.post.models import Post, Comment, Like
+from apps.user.serializers import UserSerializer
 
 
 class EditCreatePostSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Post
         fields = ["text", "image"]
@@ -14,13 +11,17 @@ class EditCreatePostSerializer(serializers.ModelSerializer):
     def validate(self, data):
         text = data.get("text")
         image = data.get("image")
-
         if not text and not image:
             raise serializers.ValidationError(
                 "At least one of Text or Image must be provided.", 400
             )
-
         return data
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = "__all__"
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -32,37 +33,52 @@ class CommentSerializer(serializers.ModelSerializer):
 
 
 class ListPostsSerializer(serializers.ModelSerializer):
-    comments = CommentSerializer(many=True, read_only=True)
     user = UserSerializer(read_only=True)
+    comments = CommentSerializer(many=True, read_only=True)
+    has_liked = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
         fields = "__all__"
 
+    def get_has_liked(self, obj):
+        return obj.has_liked
+
 
 class CreateCommentSerializer(serializers.ModelSerializer):
-    post_id = serializers.IntegerField(write_only=True)
+    post = serializers.PrimaryKeyRelatedField(queryset=Post.objects.all())
 
     class Meta:
         model = Comment
-        fields = ["post_id", "text"]
-
-    def validate_post_id(self, value):
-        get_object_or_404(Post, id=value)
-        return value
+        fields = ["post", "text"]
 
     def create(self, validated_data):
-        post_id = validated_data.pop("post_id")
-        post = get_object_or_404(Post, id=post_id)
-        comment = Comment.objects.create(post=post, **validated_data)
-        return comment
+        return Comment.objects.create(**validated_data)
 
     def to_representation(self, instance):
         return CommentSerializer(instance).data
 
 
 class UpdateCommentSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Comment
         fields = ["text"]
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Like
+        fields = "__all__"
+
+
+class CreateLikeSerializer(serializers.ModelSerializer):
+    post = serializers.PrimaryKeyRelatedField(queryset=Post.objects.all())
+
+    class Meta:
+        model = Like
+        fields = ["post"]
+
+    def to_representation(self, instance):
+        return LikeSerializer(instance).data
