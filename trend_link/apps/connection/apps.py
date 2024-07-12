23a8cc2a1047557687c3db1f079b84699d69682b
BGabor01 <busnes14@gmail.com>
2024-07-12 12:06:08 +0200

    connections.apps: imported signals

--- trend_link/apps/connection/apps.py
+++ trend_link/apps/connection/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class ConnectionConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.connection'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.connection"
+
+    def ready(self) -> None:
+        import apps.connection.signals
