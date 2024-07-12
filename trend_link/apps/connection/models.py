b3e7ddab17d0d9229ae4990e92bf718aea90e9b7
BGabor01 <busnes14@gmail.com>
2024-07-12 12:04:37 +0200

    connection.models: Created Connection, ConnectionRequest models and added connections field to User

--- trend_link/apps/connection/models.py
+++ trend_link/apps/connection/models.py
@@ -1,1 +1,76 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Connection(models.Model):
+    initiator = models.ForeignKey(
+        User,
+        related_name="initiated",
+        on_delete=models.CASCADE,
+    )
+    receiver = models.ForeignKey(
+        User,
+        related_name="receiver",
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        app_label = "connection"
+        unique_together = ("initiator", "receiver")
+        indexes = [
+            models.Index(fields=["initiator", "receiver"]),
+        ]
+
+    def __str__(self):
+        return f"Connection({self.initiator} and {self.receiver})"
+
+
+class ConnectionRequest(models.Model):
+    STATUS_CHOICES = (
+        (0, "Pending"),
+        (1, "Accepted"),
+        (2, "Rejected"),
+    )
+    sender = models.ForeignKey(
+        User,
+        related_name="sender",
+        related_query_name="sender",
+        on_delete=models.CASCADE,
+    )
+    recipient = models.ForeignKey(
+        User,
+        related_name="recipient",
+        related_query_name="recipient",
+        on_delete=models.CASCADE,
+    )
+    status = models.IntegerField(choices=STATUS_CHOICES, default=0, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        app_label = "connection"
+        unique_together = ("sender", "recipient")
+        indexes = [
+            models.Index(fields=["sender", "recipient"]),
+        ]
+
+    def __str__(self):
+        return f"ConnectionRequest(from {self.sender} to {self.recipient})"
+
+    def accept(self):
+        self.status = 1
+        self.save()
+
+    def reject(self):
+        self.status = 2
+        self.save()
+
+
+User.add_to_class(
+    "connections",
+    models.ManyToManyField(
+        "self",
+        through=Connection,
+        symmetrical=True,
+    ),
+)
