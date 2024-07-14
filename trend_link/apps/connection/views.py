9c45026991afc5521a6230e81e42678d964a9f44
BGabor01 <busnes14@gmail.com>
2024-07-14 19:08:39 +0200

    connection.views: SendConnectionRequestView, ListConnectionRequests, AcceptConnectionRequestView, RejectConnectionRequestView

--- trend_link/apps/connection/views.py
+++ trend_link/apps/connection/views.py
@@ -1,7 +1,17 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from rest_framework import status
+from django.db import IntegrityError
 
-from apps.connection.serializers import SendConnectionRequestSerializer
+
+from apps.connection.serializers import (
+    SendConnectionRequestSerializer,
+    ConnectionRequestSerializer,
+    ConnectionRequestActionsSerializer,
+)
+from apps.connection.models import ConnectionRequest
 
 
 class SendConnectionRequestView(generics.CreateAPIView):
@@ -9,4 +19,46 @@ class SendConnectionRequestView(generics.CreateAPIView):
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
-        return serializer.save(sender=self.request.user)
+        try:
+            serializer.save(sender=self.request.user)
+        except IntegrityError:
+            raise ValidationError(
+                {"detail": "Connection request already exists."},
+                code=status.HTTP_400_BAD_REQUEST,
+            )
+
+
+class ListConnectionRequests(generics.ListAPIView):
+    serializer_class = ConnectionRequestSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return ConnectionRequest.objects.filter(recipient=self.request.user, status=0)
+
+
+class AcceptConnectionRequestView(generics.UpdateAPIView):
+    serializer_class = ConnectionRequestActionsSerializer
+    permission_classes = [IsAuthenticated]
+    lookup_field = "pk"
+
+    def get_queryset(self):
+        return ConnectionRequest.objects.filter(recipient=self.request.user, status=0)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.accept()
+        return Response({"status": "accepted"}, status=status.HTTP_200_OK)
+
+
+class RejectConnectionRequestView(generics.DestroyAPIView):
+    serializer_class = ConnectionRequestActionsSerializer
+    permission_classes = [IsAuthenticated]
+    lookup_field = "pk"
+
+    def get_queryset(self):
+        return ConnectionRequest.objects.filter(recipient=self.request.user, status=0)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.reject()
+        return Response(status=status.HTTP_204_NO_CONTENT)
