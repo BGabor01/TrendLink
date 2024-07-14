40d3c103ce9776e5c7f0968bfeffd5c9f669eac4
BGabor01 <busnes14@gmail.com>
2024-07-14 19:06:56 +0200

    connection.signals: Use renamed model

--- trend_link/apps/connection/signals.py
+++ trend_link/apps/connection/signals.py
@@ -1,12 +1,12 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
-from apps.connection.models import ConnectionRequest, Connection
+from apps.connection.models import ConnectionRequest, UserConnection
 
 
 @receiver(post_save, sender=ConnectionRequest)
 def create_connection(sender, instance, **kwargs):
     if instance.status == 1:
-        Connection.objects.get_or_create(
+        UserConnection.objects.get_or_create(
             initiator=instance.sender, receiver=instance.recipient
         )
