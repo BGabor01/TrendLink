3a71b49dd46189deee5f19cfaf9b0cfcff394145
BGabor01 <busnes14@gmail.com>
2024-07-14 19:06:24 +0200

    connection.serializerS: Create SendConnectionRequestSerializer with validation logic, ConnectionRequestSerializer and ConnectionRequestActionsSerializer

--- trend_link/apps/connection/serializers.py
+++ trend_link/apps/connection/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 from apps.connection.models import ConnectionRequest
+from apps.user.serializers import UserSerializer
 
 
 class SendConnectionRequestSerializer(serializers.ModelSerializer):
@@ -10,3 +11,31 @@ class SendConnectionRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = ConnectionRequest
         fields = ["recipient"]
+
+    def validate(self, data):
+        sender = self.context["request"].user
+        recipient = data.get("recipient")
+
+        if ConnectionRequest.objects.filter(
+            sender=recipient, recipient=sender
+        ).exists():
+            raise serializers.ValidationError(
+                "A reverse connection request already exists.", 400
+            )
+
+        return data
+
+
+class ConnectionRequestSerializer(serializers.ModelSerializer):
+    sender = UserSerializer(read_only=True)
+
+    class Meta:
+        model = ConnectionRequest
+        fields = "__all__"
+
+
+class ConnectionRequestActionsSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ConnectionRequest
+        fields = []
