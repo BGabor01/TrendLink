c98b6d966bc4d734416a5df432a49f4b8cb1b441
BGabor01 <busnes14@gmail.com>
2024-07-14 19:09:04 +0200

    connection.urls: Api and template view paths

--- trend_link/apps/connection/urls.py
+++ trend_link/apps/connection/urls.py
@@ -1,7 +1,37 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
-from apps.connection.views import SendConnectionRequestView
+from apps.connection.views import (
+    SendConnectionRequestView,
+    ListConnectionRequests,
+    AcceptConnectionRequestView,
+    RejectConnectionRequestView,
+)
 
 urlpatterns = [
-    path("api/send", SendConnectionRequestView.as_view(), name="send-connectionreq-api")
+    path(
+        "api/request/send/",
+        SendConnectionRequestView.as_view(),
+        name="send-connectionreq-api",
+    ),
+    path(
+        "request/list/",
+        TemplateView.as_view(template_name="connection/connection_reqs.html"),
+        name="list-connectionreq",
+    ),
+    path(
+        "api/request/list/",
+        ListConnectionRequests.as_view(),
+        name="list-connectionreq-api",
+    ),
+    path(
+        "api/request/<int:pk>/accept/",
+        AcceptConnectionRequestView.as_view(),
+        name="accept-connectionreq-api",
+    ),
+    path(
+        "api/request/<int:pk>/reject/",
+        RejectConnectionRequestView.as_view(),
+        name="reject-connectionreq-api",
+    ),
 ]
