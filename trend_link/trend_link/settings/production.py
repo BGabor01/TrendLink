5e67bb3c2ad011a6e04ae5dc84d8966af55c6c2f
BGabor01 <busnes14@gmail.com>
2024-07-15 14:08:44 +0200

    settings.production: AWS S3 config

--- trend_link/trend_link/settings/production.py
+++ trend_link/trend_link/settings/production.py
@@ -2,3 +2,24 @@ from .settings import *
 
 DEBUG = False
 ALLOWED_HOSTS = ["localhost"]
+
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+
+_AWS_EXPIRY = 60 * 60 * 24 * 7
+AWS_S3_OBJECT_PARAMETERS = {
+    "CacheControl": f"max-age={_AWS_EXPIRY}, s-maxage={_AWS_EXPIRY}, must-revalidate"
+}
+AWS_S3_REGION_NAME = "us-east-1"
+
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+AWS_DEFAULT_ACL = None
+
+AWS_STATIC_LOCATION = "static"
+STATICFILES_STORAGE = "trend_link.storages.StaticRootS3Boto3Storage"
+STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_STATIC_LOCATION}/"
+
+AWS_PUBLIC_MEDIA_LOCATION = "media/public"
+DEFAULT_FILE_STORAGE = "trend_link.storages.MediaRootS3Boto3Storage"
+MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_PUBLIC_MEDIA_LOCATION}/"
