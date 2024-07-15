4f27dc326d980f33b82588c4082464961641d31e
BGabor01 <busnes14@gmail.com>
2024-07-15 08:00:30 +0200

    user.signals: On signup creates the UserConnection object for the user

--- trend_link/apps/user/signals.py
+++ trend_link/apps/user/signals.py
@@ -1,11 +1,13 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from apps.connection.models import UserConnection
 
 
 @receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
+def create_profile_and_connection_list(sender, instance, created, **kwargs):
     from apps.user.models import UserProfile
 
     if created:
         UserProfile.objects.create(user=instance)
+        UserConnection.objects.create(user=instance)
