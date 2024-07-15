24664bbdb64734fc342128f560277b37ace5badd
BGabor01 <busnes14@gmail.com>
2024-07-15 18:43:02 +0200

    connection.admin: Register models

--- trend_link/apps/connection/admin.py
+++ trend_link/apps/connection/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.connection.models import UserConnection, ConnectionRequest
+
+admin.site.register(UserConnection)
+admin.site.register(ConnectionRequest)
