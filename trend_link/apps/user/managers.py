0f318a9ca3e58aa559c7d9d4650a19b32553c482
BGabor01 <busnes14@gmail.com>
2024-07-15 08:01:42 +0200

    user.managers: with_connection_info checks if the users are connected

--- trend_link/apps/user/managers.py
+++ trend_link/apps/user/managers.py
@@ -1,28 +1,22 @@
 from django.contrib.auth.models import UserManager
 from django.db import models
-from django.db.models import Case, When, Q, Value, BooleanField, Exists
+from django.db.models import Q, Exists
 from apps.connection.models import ConnectionRequest
+from apps.connection.models import UserConnection
 
 
 class UserManager(UserManager):
     def with_connection_info(self, user1, user2):
-        return (
-            self.select_related("profile")
-            .prefetch_related("connections")
-            .annotate(
-                connected=Case(
-                    When(
-                        Q(connections__id=user2),
-                        then=Value(True),
-                    ),
-                    default=Value(False),
-                    output_field=BooleanField(),
-                ),
-                pending=Exists(
-                    ConnectionRequest.objects.filter(
-                        Q(sender=user1, recipient=user2, status=0)
-                        | Q(sender=user2, recipient=user1, status=0)
-                    )
-                ),
-            )
+        return self.select_related("profile").annotate(
+            pending=Exists(
+                ConnectionRequest.objects.filter(
+                    Q(sender=user1, recipient=user2, status=0)
+                    | Q(sender=user2, recipient=user1, status=0)
+                )
+            ),
+            connected=Exists(
+                UserConnection.objects.filter(
+                    Q(user=user1, connections__connections=user2)
+                )
+            ),
         )
