c8fda45de2fbab7f46171768d1d9d8cd8899f079
BGabor01 <busnes14@gmail.com>
2024-07-15 08:00:05 +0200

    connection.signals: create_connection gets the UserConnection object and add the recipient to the connection list

--- trend_link/apps/connection/signals.py
+++ trend_link/apps/connection/signals.py
@@ -1,12 +1,12 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
-from apps.connection.models import ConnectionRequest, UserConnection
+from apps.connection.models import ConnectionRequest
+from apps.connection.models import UserConnection
 
 
 @receiver(post_save, sender=ConnectionRequest)
 def create_connection(sender, instance, **kwargs):
     if instance.status == 1:
-        UserConnection.objects.get_or_create(
-            initiator=instance.sender, receiver=instance.recipient
-        )
+        connection_list = UserConnection.objects.get(user=instance.sender)
+        connection_list.connections.add(instance.recipient)
