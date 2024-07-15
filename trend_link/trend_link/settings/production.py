215b704811ebe5dde2814897b1ab9badb1c9eaa6
BGabor01 <busnes14@gmail.com>
2024-07-15 15:28:39 +0200

    settings.production: Refactored

--- trend_link/trend_link/settings/production.py
+++ trend_link/trend_link/settings/production.py
@@ -6,12 +6,12 @@ ALLOWED_HOSTS = ["localhost"]
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME")
 
 _AWS_EXPIRY = 60 * 60 * 24 * 7
 AWS_S3_OBJECT_PARAMETERS = {
     "CacheControl": f"max-age={_AWS_EXPIRY}, s-maxage={_AWS_EXPIRY}, must-revalidate"
 }
-AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME")
 
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 AWS_DEFAULT_ACL = None
