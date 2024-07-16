03c0d53f86d2e9e9c7d23892e21eecf594bacc06
BGabor01 <busnes14@gmail.com>
2024-07-16 18:09:17 +0200

    connection.signals: create_connection->create_connection_and_send_signal, can send 3 type of noti

--- trend_link/apps/connection/signals.py
+++ trend_link/apps/connection/signals.py
@@ -1,12 +1,44 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+
 from apps.connection.models import ConnectionRequest
 from apps.connection.models import UserConnection
 
 
 @receiver(post_save, sender=ConnectionRequest)
-def create_connection(sender, instance, **kwargs):
-    if instance.status == 1:
-        connection_list = UserConnection.objects.get(user=instance.sender)
-        connection_list.connections.add(instance.recipient)
+def create_connection_and_send_signal(sender, instance, **kwargs):
+    match instance.status:
+        case 0:
+            channel_layer = get_channel_layer()
+            async_to_sync(channel_layer.group_send)(
+                f"user_{instance.recipient.id}",
+                {
+                    "type": "notification_message",
+                    "message": f"{instance.sender} wants to connect with you!",
+                },
+            )
+
+        case 1:
+            connection_list = UserConnection.objects.get(user=instance.sender)
+            connection_list.connections.add(instance.recipient)
+            channel_layer = get_channel_layer()
+            async_to_sync(channel_layer.group_send)(
+                f"user_{instance.sender.id}",
+                {
+                    "type": "notification_message",
+                    "message": f"{instance.recipient} accepted your connection request!",
+                },
+            )
+
+        case 2:
+            channel_layer = get_channel_layer()
+            async_to_sync(channel_layer.group_send)(
+                f"user_{instance.sender.id}",
+                {
+                    "type": "notification_message",
+                    "message": f"{instance.recipient} rejected your connection request!",
+                },
+            )
