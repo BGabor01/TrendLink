8fadd9fbf9115970d5ce05335d263e0a1790aef8
BGabor01 <busnes14@gmail.com>
2024-07-16 20:54:05 +0200

    user.tests: using rest framework's ApiClient

--- trend_link/apps/user/tests.py
+++ trend_link/apps/user/tests.py
@@ -1,12 +1,8 @@
 import pytest
 from django.contrib.auth.models import User
-from django.test import RequestFactory
-from rest_framework.test import force_authenticate
-from rest_framework import generics, mixins
-from rest_framework.permissions import IsAuthenticated
+from django.urls import reverse
+from rest_framework.test import APIClient
 
-from apps.user.permissions import IsOwnerOrReadOnly
-from apps.user.serializers import ProfileSerializer
 from apps.user.models import UserProfile
 
 
@@ -15,62 +11,33 @@ def user(db):
     return User.objects.create_user(username="testuser", password="password")
 
 
-@pytest.mark.django_db
-def test_profile_creating_signal(user):
-    from apps.user.models import UserProfile
-
-    profile = UserProfile.objects.get(user=user)
-    assert profile
-
-
-class TestView(
-    mixins.RetrieveModelMixin, mixins.UpdateModelMixin, generics.GenericAPIView
-):
-    serializer_class = ProfileSerializer
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
-    queryset = UserProfile.objects.all()
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-
 @pytest.fixture
 def other_user(db):
     return User.objects.create_user(username="other", password="password")
 
 
 @pytest.fixture
-def factory():
-    return RequestFactory()
+def client():
+    return APIClient()
 
 
-def test_permission_denied_if_not_owner_PUT(factory, other_user, user):
-    from apps.user.models import UserProfile
-
-    data = {"bio": "Updated bio", "birth_date": "2000-01-01"}
-    request = factory.put("/fake-url", data, content_type="application/json")
-    request.user = other_user
+@pytest.mark.django_db
+def test_profile_creating_signal(user):
     profile = UserProfile.objects.get(user=user)
+    assert profile
 
-    view = TestView.as_view()
-    force_authenticate(request, user=other_user)
-    response = view(request, pk=profile.pk)
+
+def test_permission_denied_if_not_owner_PUT(client, other_user):
+    client.login(username="testuser", password="password")
+    url = reverse(f"profile-update-api", kwargs={"pk": other_user.id})
+    data = {"bio": "Updated bio", "birth_date": "2000-01-01"}
+    response = client.put(url, data, format="json")
     assert response.status_code == 403
 
 
-def test_access_granted_GET(factory, user):
-    from apps.user.models import UserProfile
+def test_access_granted_GET(client, user):
+    client.login(username="testuser", password="password")
+    url = reverse(f"profile-retrieve-api", kwargs={"pk": user.id})
+    response = client.get(url)
 
-    request = factory.get("/fake-url")
-    request.user = user
-    profile = UserProfile.objects.get(user=user)
-
-    view = TestView.as_view()
-    force_authenticate(request, user=user)
-    response = view(request, pk=profile.pk)
     assert response.status_code == 200
-
-
