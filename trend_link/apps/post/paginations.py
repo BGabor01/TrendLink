3c28ed3e28bb20e4b078304fa94b376447961067
BGabor01 <busnes14@gmail.com>
2024-07-17 21:31:57 +0200

    post.paginations: cursor paginations for comments and posts

--- trend_link/apps/post/paginations.py
+++ trend_link/apps/post/paginations.py
@@ -1,13 +1,21 @@
 from rest_framework import pagination
+from rest_framework.response import Response
 
 
-class CommentPagination(pagination.PageNumberPagination):
-    page_size = 5
+class CommentPagination(pagination.CursorPagination):
+    page_size = 2
+    ordering="-created_at"
 
     def get_paginated_response(self, data):
-        return {
-            "count": self.page.paginator.count,
-            "next": self.get_next_link(),
-            "previous": self.get_previous_link(),
-            "results": data,
-        }
+        return Response(
+            {
+                "next": self.get_next_link(),
+                "previous": self.get_previous_link(),
+                "results": data,
+            }
+        )
+
+
+class PostCursorPagination(pagination.CursorPagination):
+    page_size = 10
+    ordering = "-created_at"
