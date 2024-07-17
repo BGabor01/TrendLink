bcb8e77e625dd8b24d855dbfcedc65b50849e1d8
BGabor01 <busnes14@gmail.com>
2024-07-17 22:25:12 +0200

    post.create-post.js: Append tags to formData

--- trend_link/apps/post/static/post/js/create-post.js
+++ trend_link/apps/post/static/post/js/create-post.js
@@ -1,10 +1,20 @@
-$(document).ready(function() {
-    $('#postForm').on('submit', function(e) {
+document.addEventListener("DOMContentLoaded", function () {
+    const input = document.querySelector('#tags');
+    const tagify = new Tagify(input);
+
+    $('#postForm').on('submit', function (e) {
         e.preventDefault();
-        var $form = $(this);
-        var url = $form.data('url');
-        var formData = new FormData(this);
-        console.log(url)
+        const $form = $(this);
+        const url = $form.data('url');
+        const formData = new FormData(this);
+
+        const tagsArray = tagify.value.map(tag => tag.value);
+        formData.delete('tags');
+        tagsArray.forEach(tag => {
+            formData.append('tags', tag);
+        });
+
+        console.log(tagsArray);
 
         $.ajax({
             type: 'POST',
@@ -15,19 +25,17 @@ $(document).ready(function() {
             headers: {
                 'X-CSRFToken': $('input[name="csrfmiddlewaretoken"]').val()
             },
-            success: function(response) {
+            success: function (response) {
                 const redirectUrl = localStorage.getItem('redirectAfterLogin');
                 if (redirectUrl !== null) {
                     localStorage.removeItem('redirectAfterLogin');
                     window.location.href = redirectUrl;
-                } else {
-                    window.location.href = "/home";
                 }
             },
-            error: function(response) {
-                var errors = response.responseJSON;
-                var errorMessage = '';
-                for (var field in errors) {
+            error: function (response) {
+                const errors = response.responseJSON;
+                let errorMessage = '';
+                for (const field in errors) {
                     errorMessage += errors[field] + ' ';
                 }
                 $('#error-message').text(errorMessage);
