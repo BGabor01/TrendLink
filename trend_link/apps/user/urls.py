20b11659662c98b2938dcf6d9ddb5cf7b34ddd5f
BGabor01 <busnes14@gmail.com>
2024-07-23 17:08:24 +0200

    user.urls: Renamed endpoints

--- trend_link/apps/user/urls.py
+++ trend_link/apps/user/urls.py
@@ -10,32 +10,23 @@ from .views import (
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("api/singup/", SignUpView.as_view(), name="signup-api"),
-    path(
-        "signup/", TemplateView.as_view(template_name="user/signup.html"), name="signup"
-    ),
-    path("api/login/", LoginView.as_view(), name="login-api"),
-    path("login/", TemplateView.as_view(template_name="user/login.html"), name="login"),
-    path(
-        "api/profile/<int:pk>/update/",
-        UpdateProfileView.as_view(),
-        name="profile-update-api",
-    ),
-    path(
-        "api/profile/<int:pk>/",
-        RetrieveProfileView.as_view(),
-        name="profile-retrieve-api",
-    ),
-    path(
-        "profile/<int:pk>/",
-        TemplateView.as_view(template_name="user/profile.html"),
-        name="profile",
-    ),
-    path("logout/", LogoutView.as_view(), name="logout"),
-    path("api/members/", ListMembersView.as_view(), name="members-api"),
-    path(
-        "members/",
-        TemplateView.as_view(template_name="user/members.html"),
-        name="members",
-    ),
+    # API Endpoints
+    path("api/signup/", SignUpView.as_view(), name="api_signup"),
+    path("api/login/", LoginView.as_view(), name="api_login"),
+    path("api/profile/<int:pk>/update/",
+         UpdateProfileView.as_view(), name="api_profile_update"),
+    path("api/profile/<int:pk>/", RetrieveProfileView.as_view(),
+         name="api_profile_detail"),
+    path("api/members/", ListMembersView.as_view(), name="api_members_list"),
+
+    # Template Views
+    path("signup/", TemplateView.as_view(template_name="user/signup.html"),
+         name="view_signup"),
+    path("login/", TemplateView.as_view(template_name="user/login.html"),
+         name="view_login"),
+    path("profile/<int:pk>/",
+         TemplateView.as_view(template_name="user/profile.html"), name="view_profile"),
+    path("members/", TemplateView.as_view(template_name="user/members.html"),
+         name="view_members"),
+    path("logout/", LogoutView.as_view(), name="view_logout"),
 ]
