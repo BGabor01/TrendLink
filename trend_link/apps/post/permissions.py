40fd33cf2c222a05c0225d49282eefb92844d93b
BGabor01 <busnes14@gmail.com>
2024-07-23 17:52:32 +0200

    permission files: Docstrings

--- trend_link/apps/post/permissions.py
+++ trend_link/apps/post/permissions.py
@@ -2,6 +2,10 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class IsOwnerOrPostOwnerOrReadOnly(BasePermission):
+    """
+    Custom permission to only allow owners of an object or the owner of the associated post to edit it.
+    Read-only access is allowed for non-owners.
+    """
 
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
--- trend_link/apps/user/permissions.py
+++ trend_link/apps/user/permissions.py
@@ -2,6 +2,10 @@ from rest_framework import permissions
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    Read-only access is allowed for non-owners.
+    """
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
