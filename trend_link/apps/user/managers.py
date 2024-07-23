082418113ac3190d7fb0c0e2771703a5a477437b
BGabor01 <busnes14@gmail.com>
2024-07-23 17:54:09 +0200

    user.managers: Cleaned up and added docstrings

--- trend_link/apps/user/managers.py
+++ trend_link/apps/user/managers.py
@@ -1,17 +1,24 @@
-from django.contrib.auth.models import UserManager
-from django.db import models
-from django.db.models import Q, Exists
-from apps.connection.models import ConnectionRequest
-from apps.connection.models import UserConnection
+from django.db.models import Exists, Q
+from django.contrib.auth.models import UserManager as BaseUserManager
+
+from apps.connection.models import ConnectionRequest, UserConnection
 
 
-class UserManager(UserManager):
+class UserManager(BaseUserManager):
+    """
+    Custom manager for the User model.
+    """
+
     def with_connection_info(self, user1, user2):
+        """
+        Annotate users with connection information between user1 and user2.
+        Adds `pending` and `connected` annotations to indicate the connection status.
+        """
         return self.select_related("profile").annotate(
             pending=Exists(
                 ConnectionRequest.objects.filter(
-                    Q(sender=user1, recipient=user2, status=0)
-                    | Q(sender=user2, recipient=user1, status=0)
+                    Q(sender=user1, recipient=user2, status=0) |
+                    Q(sender=user2, recipient=user1, status=0)
                 )
             ),
             connected=Exists(
