e18d9fed60efe96da2ef821f32cfef6ecde2fc85
BGabor01 <busnes14@gmail.com>
2024-07-23 17:12:45 +0200

    post.urls: Refactored, new url names

--- trend_link/apps/post/urls.py
+++ trend_link/apps/post/urls.py
@@ -13,30 +13,22 @@ from apps.post.views import (
 )
 
 urlpatterns = [
-    path("api/create/", CreatePostView.as_view(), name="create-post-api"),
-    path(
-        "create/",
-        TemplateView.as_view(template_name="post/post.html"),
-        name="create-post",
-    ),
-    path("api/list/", ListPostsView.as_view(), name="list-post-api"),
-    path("api/<int:pk>/update/", UpdatePostView.as_view(), name="update-post-api"),
-    path("api/<int:pk>/delete/", DeletePostView.as_view(), name="delete-post-api"),
-    path("api/comment/create/", CreateCommentView.as_view(), name="create-comment-api"),
-    path(
-        "api/comment/<int:pk>/delete/",
-        DeleteCommentView.as_view(),
-        name="delete-comment-api",
-    ),
-    path(
-        "api/comment/<int:pk>/update/",
-        UpdateCommentView.as_view(),
-        name="update-comment-api",
-    ),
-    path("api/like/create/", LikePostView.as_view(), name="like-post-api"),
-    path(
-        "api/like/<int:post_id>/remove/",
-        UnlikePostView.as_view(),
-        name="unlike-post-api",
-    ),
+    # API Endpoints
+    path("api/create/", CreatePostView.as_view(), name="api_create_post"),
+    path("api/list/", ListPostsView.as_view(), name="api_list_posts"),
+    path("api/<int:pk>/update/", UpdatePostView.as_view(), name="api_update_post"),
+    path("api/<int:pk>/delete/", DeletePostView.as_view(), name="api_delete_post"),
+    path("api/comment/create/", CreateCommentView.as_view(),
+         name="api_create_comment"),
+    path("api/comment/<int:pk>/delete/",
+         DeleteCommentView.as_view(), name="api_delete_comment"),
+    path("api/comment/<int:pk>/update/",
+         UpdateCommentView.as_view(), name="api_update_comment"),
+    path("api/like/create/", LikePostView.as_view(), name="api_like_post"),
+    path("api/like/<int:post_id>/remove/",
+         UnlikePostView.as_view(), name="api_unlike_post"),
+
+    # Template Views
+    path("create/", TemplateView.as_view(template_name="post/post.html"),
+         name="view_create_post"),
 ]
