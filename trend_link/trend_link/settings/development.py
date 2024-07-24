2cd8d07e69c7078e4fe343ca74f425f608174deb
BGabor01 <busnes14@gmail.com>
2024-07-24 15:34:31 +0200

    settings.development: Debug toolbar settings

--- trend_link/trend_link/settings/development.py
+++ trend_link/trend_link/settings/development.py
@@ -1,1 +1,9 @@
 from .settings import *
+
+INSTALLED_APPS += ["debug_toolbar"]
+
+MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': lambda _request: DEBUG
+}
